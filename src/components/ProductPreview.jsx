a55072084e85f18acd1eccb9b16367870dda60d1
Dung <dudau0924222@gmail.com>
2022-11-23 21:11:04 -0500

    refactor with css reset, css variables, utility classes and changes to various css classes

--- src/components/ProductPreview.jsx
+++ src/components/ProductPreview.jsx
@@ -4,22 +4,30 @@ import './ProductPreview.css';
 const productPreview = () => {
   return (
     <div className="product-preview-container">
-      <div className="product-preview-content-container">
-        <img
-          src={require('../images/image-product-desktop.jpg')}
-          alt="product"
-          id="product-preview-img"
-        />
-        <div className="product-preview-text">
-          <p id="perfume">P E R F U M E</p>
-          <h1>Gabrielle Essence Eau De Parfum</h1>
-          <p>
+      <div className="product-preview-text">
+        <div>
+          <img
+            src={require('../images/image-product-mobile.jpg')}
+            alt="product"
+            id="product-preview-img"
+          />
+        </div>
+        <div className="flow product-preview-text-content">
+          <h1 class="ff-montserrat fs-200 letter-spacing-1 text-gray">
+            PERFUME
+          </h1>
+          <h2 className="fs-700 bold line-height-1 text-black">
+            Gabrielle Essence Eau De Parfum
+          </h2>
+          <p className="text-gray">
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
-          <b className="product-preview-current-price">$149.99</b>
-          <s className="product-preview-old-price">$169.99</s>
-          <button className="primary-button">Add to Cart</button>
+          <b className="fs-700 text-dark-cyan product-preview-current-price">
+            $149.99
+          </b>
+          <s className="text-gray">$169.99</s>
+          <button className="primary-button block-display">Add to Cart</button>
         </div>
       </div>
     </div>
